cfdad50d75d4de05a08dc445507d683f5694d5ee
aklin <a.klinakis@yahoo.com>
2015-03-10 03:08:08 +0000

    Removed old code
    A massive amount of wasted effort was just done away with. There's some refactoring to be done on the copy-pasted code, but that's for another day.

--- src/main/java/ovh/ebis/geezer/library/CaptchaCom.java
+++ src/main/java/ovh/ebis/geezer/library/CaptchaCom.java
@@ -4,29 +4,16 @@ import com.DeathByCaptcha.Captcha;
 import com.DeathByCaptcha.Client;
 import com.DeathByCaptcha.SocketClient;
 import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import com.mashape.unirest.request.GetRequest;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.ArrayDeque;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import javax.xml.bind.DatatypeConverter;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.openqa.selenium.*;
 import ovh.ebis.geezer.ArgumentNumberException;
 import ovh.ebis.geezer.InvalidTargetException;
-import ovh.ebis.geezer.Quiver;
 import ovh.ebis.geezer.SourceParser;
 
 /**
@@ -36,28 +23,13 @@ import ovh.ebis.geezer.SourceParser;
  */
 public class CaptchaCom extends Command {
 
-	/**
-	 * Max attempts to poll the server for result.
-	 */
-	private static final int TRIES;
-	/**
-	 * Initial delay before, and seconds between, each attempt.
-	 */
-	private static final int TIMEOUT;
-
-	private static final String URL;
-
 	private static final String USERNAME;
-
 	private static final String PASSWORD;
 
 	private final By challenge;
 	private final By response;
 
 	static {
-		TRIES = 5;
-		TIMEOUT = TRIES * 10;
-		URL = "http://api.dbcapi.me/api/captcha";
 		USERNAME = "hellocaptcha";
 		PASSWORD = "hellocaptcha";
 	}
@@ -103,8 +75,6 @@ public class CaptchaCom extends Command {
 		final BufferedImage sshot;
 		final HttpResponse<String> resp;
 		final WebElement e;
-//		final String b64img;
-		final String cid; //captcha ID. Returned by the server after step 1.
 
 		e = getDriver().findElement(eid);
 
@@ -117,28 +87,13 @@ public class CaptchaCom extends Command {
 		}
 
 		//debug
-		System.out.println("\tE: " + eid.toString());
-//		saveImage(sshot);
 		return fire(saveImage(sshot));
 
-//		System.exit(0);
-		//Step 0 is done. Now to steop 1: Send screengrab to captcha solver
-//		resp = submitChallenge(saveImage(sshot));
-//
-//		try {
-//			cid = resp.getBody();//.getObject().getString("captcha");
-//
-//			System.out.println("\t***Got CID: " + cid);
-//		} catch (JSONException ex) {
-//			return null;
-//		}
-//		return pollLoop(cid);          //total attempts
 	}
 
 	private String fire(final File challenge) {
 		// Put your DBC username & password here:
-		//Client client = (Client)(new HttpClient(args[0], args[1]));
-		Client client = (Client) (new SocketClient(USERNAME, PASSWORD));
+		final Client client = new SocketClient(USERNAME, PASSWORD);
 		client.isVerbose = true;
 
 		try {
@@ -187,31 +142,6 @@ public class CaptchaCom extends Command {
 	}
 
 	/**
-	 * Submits a CAPTCHA challenge request to the server. This is Stage 1 of the
-	 * solving process; we still need to poll the server for the result.
-	 * @param img
-	 * @return
-	 */
-//	private HttpResponse<String> submitChallenge(final File img) {
-//		final HttpResponse<String> resp;
-////		final String b64img;
-//
-////		b64img = "base64:" + DatatypeConverter.printBase64Binary(img);
-//		try {
-//			resp = Unirest.post(URL)
-//				.header("accept", "application/json")
-//				.queryString("username", USERNAME)
-//				.field("password", PASSWORD)
-//				.field("image", img)
-//				.asString();
-//		} catch (UnirestException ex) {
-//			System.err.println("REST problem: " + ex);
-//			return null;
-//		}
-//
-//		return resp;
-//	}
-	/**
 	 * Dumps the image in a randomly-named file. This is useful for later
 	 * diagnostics or wrong solutions.
 	 * @param img
@@ -230,101 +160,6 @@ public class CaptchaCom extends Command {
 		return out;
 	}
 
-	/**
-	 * Step 2: Poll captcha service for result.
-	 * @param id Captcha ID (returned from step 1)
-	 */
-//	private String pollLoop(final String id) {
-//		final GetRequest b;
-//		ImmutablePair<Boolean, String> ret = null;
-//
-//		b = Unirest.get(
-//			new StringBuilder()
-//			.append(URL)
-//			.append("/")
-//			.append(id)
-//			.toString());
-//
-//		snooze(TIMEOUT); //initial snooze
-//
-//		for (int i = 0; i < TRIES; i++) {
-//			ret = pollSingle(b);
-//
-//			if (ret == null) {//some type of error occured
-//				System.err.println("CAPTCHA Abort: Error solving " + id);
-//				return null;
-//			}
-//
-//			if (ret.getLeft() == true) {
-//				// Return the solved string after some error checking
-//				if (ret.getRight() == null)
-//					throw new NullPointerException("Something very trippy happened");
-//				return ret.getRight(); //We got the captcha
-//			} else
-//				snooze(TIMEOUT); //Poll failed. Wait for next iteration
-//		}
-//
-//		return null; //We got nada.
-//	}
-	/**
-	 * Does the dirty work of pollLoop. Specifically, it polls the captcha
-	 * service for a given ID. A boolean-String pair is returned based on the
-	 * server's response. A boolean 'true' signifies that we got an HTTP 200
-	 * response, as well as the solved CAPTCHA string. If the boolean part is
-	 * 'false', the String part will be 'null', which means that the solution is
-	 * not yet available. This method will return null on a HTTP response other
-	 * than 200. The appropriate error message will be written in stderr.
-	 * @param id CAPTCHA ID to poll for
-	 * @param rest Pre-prepared request to execute
-	 * @return true+String on solve, false+null on not-available, null on error
-	 */
-//	private ImmutablePair<Boolean, String> pollSingle(final GetRequest rest) {
-//		final HttpResponse<JsonNode> reply;
-//		try {
-//			reply = rest.asJson();
-//		} catch (UnirestException ex) {
-//			System.err.println("REST exception");
-//			return null;
-//		}
-//
-//		final JSONObject j = reply.getBody().getObject();
-//
-//		//check result
-//		switch (reply.getStatus()) {
-//			case 200: // HTTP 200: All is good
-//				try {
-//					if (j.getBoolean("is_correct"))
-//						return new ImmutablePair<>(true, j.
-//												   getString("text"));
-//					else
-//						return new ImmutablePair<>(false, null);
-//				} catch (JSONException ex) {
-//					System.err.
-//						println("JSON Exception while reading CAPTCHA response:" + ex);
-//				}
-//				break;
-//			case 404:
-//				System.err.println("Error: Got 404");
-//				return null;
-//			default:
-//				System.err.println("Error: Unrecognized HTTP code: "
-//					+ reply.getStatus());
-//				return null;
-//		}
-//		return null;
-//	}
-	/**
-	 * Puts the calling thread to sleep for a given number of seconds.
-	 * @param sec
-	 */
-//	private void snooze(final int sec) {
-//		if (sec > 0)
-//			try {
-//				Thread.sleep(sec * 1000);
-//			} catch (InterruptedException ex) {
-//				Thread.currentThread().interrupt();
-//			}
-//	}
 	/**
 	 * Take a screenshot of a given WebElement.
 	 * @param e
@@ -357,19 +192,4 @@ public class CaptchaCom extends Command {
 							   bottomright.getY());
 	}
 
-	/**
-	 * Accepts a BufferedImage and returns a byte array that contains its data.
-	 * @param img
-	 * @return byte[] the image as a byte array
-	 */
-//	private byte[] getBytesFromImage(final BufferedImage img) {
-//		final byte[] ret;
-//		try (final ByteArrayOutputStream b = new ByteArrayOutputStream()) {
-//			ImageIO.write(img, "jpg", b);
-//			ret = b.toByteArray();
-//		} catch (IOException ex) {
-//			return null;
-//		}
-//		return ret;
-//	}
 }
