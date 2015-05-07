227e149bc48c1983ca6f42f5c364f118eded9f38
aklin <a.klinakis@yahoo.com>
2015-05-07 06:09:20 +0100

    SourceParser no longer parses entire files
    but individual lines/commands only.

--- src/main/java/ovh/ebis/geezer/SourceParser.java
+++ src/main/java/ovh/ebis/geezer/SourceParser.java
@@ -1,14 +1,10 @@
 package ovh.ebis.geezer;
 
 import ovh.ebis.geezer.library.Command;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.openqa.selenium.By;
-import org.openqa.selenium.lift.match.SelectionMatcher;
-import org.openqa.selenium.support.ui.Select;
 import ovh.ebis.geezer.library.ComInfo;
 
 public class SourceParser {
@@ -19,40 +15,11 @@ public class SourceParser {
 	private static final String COMMENT;
 
 	private final static Pattern comPattern;
-	private final Properties inputs;
-	private final Scanner source;
 
 	static {
 		COMMENT = "^\\s*//";
-//		COMMENT = "[^\\\"].*//.*[^\\\"]";
 		comPattern = Pattern.compile("([^\\\"]\\S+[^\\\"])|(\\\".*\\\")",
 									 Pattern.UNICODE_CHARACTER_CLASS);
-	}
-
-	public SourceParser(final Properties in, final Scanner resource) {
-		inputs = in;
-		this.source = resource;
-	}
-
-	/**
-	 * Parse a collection of lines (such as a file) and turn it into a queue of
-	 * Commands.
-	 * @return
-	 */
-	public ArrayDeque<Command> parse() {
-		final ArrayDeque<Command> ret;
-		String cur;
-		Command temp;
-		ret = new ArrayDeque<>();
-
-		while (source.hasNext()) {
-			cur = source.nextLine();
-			temp = parseLine(cur);
-			if (temp != null)
-				ret.add(temp);
-		}
-
-		return ret;
 	}
 
 	/**
@@ -61,7 +28,7 @@ public class SourceParser {
 	 * @return
 	 */
 	public static Command parseLine(String s) {
-		final Matcher m = comPattern.matcher(s);
+		final Matcher m;
 		final ArrayList<String> bag;
 		final ComInfo cinfo;
 		final Command ret;
@@ -73,6 +40,7 @@ public class SourceParser {
 		 BUG: Comment is not escaped if enclosed in double quotes
 		 */
 		s = s.split(COMMENT, 2)[0].trim();
+		m = comPattern.matcher(s);
 
 		/*
 		 Inputs consist of single groups [a-zA-Z0-9] and symbols, or same
@@ -89,7 +57,6 @@ public class SourceParser {
 				println("Parse error: \"" + m.group() + "\" not recognized");
 			return null;
 		}
-//		System.out.println("Input: " + s);
 
 		while (m.find()) {
 			group = m.group().trim();
@@ -99,7 +66,6 @@ public class SourceParser {
 		}
 
 		return Quiver.spawnCommand(bag, cinfo);
-//		return new Command(bag, cinfo);
 	}
 
 	/**
@@ -107,7 +73,7 @@ public class SourceParser {
 	 * @param test
 	 * @return
 	 */
-	public static boolean surroundedByQuotes(final String test) {
+	public static boolean isDoubleQuoted(final String test) {
 		return test.startsWith("\"") && test.endsWith("\"");
 	}
 
@@ -117,7 +83,7 @@ public class SourceParser {
 	 * @return
 	 */
 	public static String stripQuotes(final String s) {
-		if (surroundedByQuotes(s) && s.length() >= 2)
+		if (isDoubleQuoted(s) && s.length() >= 2)
 			return s.substring(1, s.length() - 1);
 		else
 			return s;
